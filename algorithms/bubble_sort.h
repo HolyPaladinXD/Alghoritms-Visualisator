f0af259221c14e73fed373da320a467f0a34f2c5
Denis <den1sflucker@gmail.com>
2025-09-22 23:05:41 +0300

    Fix Bubble Sort implementation

--- algorithms/bubble_sort.h
+++ algorithms/bubble_sort.h
@@ -1,7 +1,19 @@
 #ifndef BUBBLE_SORT_H
 #define BUBBLE_SORT_H
 
-#endif // BUBBLE_SORT_H
 #include <vector>
+#include <algorithm>
 
-template<typename T> void bubbleSort(std::vector<T>&);
+template<typename T> void bubbleSort(std::vector<T>& data){
+
+    for(int i =0; i < data.size(); ++i){
+        for(int j = 0; j +1 < data.size(); ++j){
+            if(data[j] > data[j+1]){
+                std::swap(data[j], data[j+1]);
+            }
+        }
+    }
+}
+
+#endif // BUBBLE_SORT_H
+
--- main.cpp
+++ main.cpp
@@ -1,11 +1,20 @@
 #include "mainwindow.h"
-
 #include <QApplication>
+#include "algorithms/bubble_sort.h"
+#include <QDebug>
+#include <vector>
+
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    std::vector<int> data = {5, 3, 8, 1, 2};
+    bubbleSort(data);
+    for (int v : data) {
+        qDebug() << v;
+    }
+    return 0;
     w.show();
     return a.exec();
 }
